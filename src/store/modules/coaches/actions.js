4ae3ef2aecaf06c260a0c9ee094d655e43ffcffe
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-26 04:24:45 -1200

    added register coach in firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,28 @@
 export default {
-    registerCoach(context, data) {
-      const coachData = {
-        id: context.rootGetters.userId,
-        firstName: data.first,
-        lastName: data.last,
-        description: data.desc,
-        hourlyRate: data.rate,
-        areas: data.areas
-      };
-  
-      context.commit('registerCoach', coachData);
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas
+    };
+
+    const response = await fetch(`https://vue-coachapp-1dd1d-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
     }
-  };
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
+  }
+};
